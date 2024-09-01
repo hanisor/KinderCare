0993eac46aa3a665f8417e026c844bb04f5327d2
hanisor <hanisorhana@gmail.com>
2024-09-01 21:52:45 +0800

    updated

--- lib/caregiverScreen/caregiver_sick.dart
+++ lib/caregiverScreen/caregiver_sick.dart
@@ -20,7 +20,8 @@ class _CaregiverSicknessState extends State<CaregiverSickness> {
   List<SicknessModel> checklistItems = [];
 
   Future<void> fetchChecklistItems() async {
-    RequestController req = RequestController(path: 'sickness-data/${widget.caregiverId}');
+    RequestController req =
+        RequestController(path: 'sickness-data/${widget.caregiverId}');
     await req.get();
     var response = req.result();
     print("raw response: $response");
@@ -30,7 +31,7 @@ class _CaregiverSicknessState extends State<CaregiverSickness> {
       DateTime now = DateTime.now();
 
       setState(() {
-        checklistItems.clear();  // Clear the list before adding new items
+        checklistItems.clear(); // Clear the list before adding new items
       });
 
       for (var childGroup in childGroupList) {
@@ -39,11 +40,14 @@ class _CaregiverSicknessState extends State<CaregiverSickness> {
           List<dynamic> sicknessesList = child['sicknesses'];
           ChildModel childModel = ChildModel.fromJson(child);
           for (var sickness in sicknessesList) {
-            SicknessModel sicknessModel = SicknessModel.fromJson(sickness, childModel: childModel);
+            SicknessModel sicknessModel =
+                SicknessModel.fromJson(sickness, childModel: childModel);
 
             // Filter by current date
             DateTime sicknessDate = DateTime.parse(sicknessModel.dateTime);
-            if (sicknessDate.year == now.year && sicknessDate.month == now.month && sicknessDate.day == now.day) {
+            if (sicknessDate.year == now.year &&
+                sicknessDate.month == now.month &&
+                sicknessDate.day == now.day) {
               setState(() {
                 checklistItems.add(sicknessModel);
               });
@@ -56,7 +60,8 @@ class _CaregiverSicknessState extends State<CaregiverSickness> {
       checklistItems.sort((a, b) {
         if (a.sicknessStatus == 'Pending' && b.sicknessStatus == 'Taken') {
           return -1;
-        } else if (a.sicknessStatus == 'Taken' && b.sicknessStatus == 'Pending') {
+        } else if (a.sicknessStatus == 'Taken' &&
+            b.sicknessStatus == 'Pending') {
           return 1;
         }
         return 0;
@@ -64,20 +69,23 @@ class _CaregiverSicknessState extends State<CaregiverSickness> {
     }
   }
 
-  Future<void> updateSicknessStatus(int? sicknessId) async {
+  Future<void> updateSicknessStatus(int? sicknessId, bool isTaken) async {
     Map<String, dynamic> requestBody = {};
-    if (sicknessStatus == "Pending") {
+
+    if (isTaken) {
       requestBody["status"] = "Taken";
-      sicknessStatus = "Taken";
+    } else {
+      requestBody["status"] = "Pending";
     }
 
-    RequestController req = RequestController(path: 'caregiver/update-sickness/$sicknessId');
+    RequestController req =
+        RequestController(path: 'caregiver/update-sickness/$sicknessId');
     req.setBody(requestBody);
     await req.put();
 
     if (req.status() == 200) {
       Fluttertoast.showToast(
-        msg: 'Update successfully',
+        msg: 'Update successful',
         backgroundColor: Colors.white,
         textColor: Colors.red,
         gravity: ToastGravity.CENTER,
@@ -110,82 +118,84 @@ class _CaregiverSicknessState extends State<CaregiverSickness> {
   }
 
   @override
-Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      title: Text('Sickness checklist'),
-    ),
-    body: RefreshIndicator(
-      onRefresh: _refreshData,
-      child: checklistItems.isEmpty
-          ? Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(
-                    Icons.sentiment_dissatisfied,
-                    size: 100,
-                    color: Colors.grey,
-                  ),
-                  SizedBox(height: 16),
-                  Text(
-                    'No sickness checklist available',
-                    style: TextStyle(
-                      fontSize: 18,
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sickness checklist'),
+      ),
+      body: RefreshIndicator(
+        onRefresh: _refreshData,
+        child: checklistItems.isEmpty
+            ? Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.sentiment_dissatisfied,
+                      size: 100,
                       color: Colors.grey,
                     ),
-                  ),
-                ],
-              ),
-            )
-          : ListView.builder(
-              itemCount: checklistItems.length,
-              itemBuilder: (context, index) {
-                SicknessModel item = checklistItems[index];
-                return Card(
-                  elevation: 4,
-                  margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-                  child: ListTile(
-                    title: RichText(
-                      text: TextSpan(
-                        style: DefaultTextStyle.of(context).style,
-                        children: <TextSpan>[
-                          const TextSpan(
-                            text: 'Child Name: ',
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          TextSpan(
-                            text: '${item.childModel?.childName ?? "Unknown"}',
-                            style: TextStyle(fontWeight: FontWeight.normal),
-                          ),
-                        ],
+                    SizedBox(height: 16),
+                    Text(
+                      'No sickness checklist available',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.grey,
                       ),
                     ),
-                    subtitle: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('Sickness Type: ${item.sicknessType}'),
-                        Text('Dosage: ${item.dosage}'),
-                        Text('Date and Time: ${_formatDateTime(item.dateTime)}'),
-                        Text('Status: ${item.sicknessStatus}'),
-                      ],
+                  ],
+                ),
+              )
+            : ListView.builder(
+                itemCount: checklistItems.length,
+                itemBuilder: (context, index) {
+                  SicknessModel item = checklistItems[index];
+                  return Card(
+                    elevation: 4,
+                    margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                    child: ListTile(
+                      title: RichText(
+                        text: TextSpan(
+                          style: DefaultTextStyle.of(context).style,
+                          children: <TextSpan>[
+                            const TextSpan(
+                              text: 'Child Name: ',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            TextSpan(
+                              text:
+                                  '${item.childModel?.childName ?? "Unknown"}',
+                              style: TextStyle(fontWeight: FontWeight.normal),
+                            ),
+                          ],
+                        ),
+                      ),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Sickness Type: ${item.sicknessType}'),
+                          Text('Dosage: ${item.dosage}'),
+                          Text(
+                              'Date and Time: ${_formatDateTime(item.dateTime)}'),
+                          Text('Status: ${item.sicknessStatus}'),
+                        ],
+                      ),
+                      trailing: Checkbox(
+                        value: item.sicknessStatus == 'Taken',
+                        onChanged: (bool? value) {
+                          setState(() {
+                            item.sicknessStatus = value! ? 'Taken' : 'Pending';
+                          });
+                          updateSicknessStatus(item.sicknessId, value!);
+                        },
+                      ),
                     ),
-                    trailing: Checkbox(
-                      value: item.sicknessStatus == 'Taken',
-                      onChanged: (bool? value) {
-                        setState(() {
-                          item.sicknessStatus = value! ? 'Taken' : 'Pending';
-                        });
-                        updateSicknessStatus(item.sicknessId);
-                      },
-                    ),
-                  ),
-                );
-              },
-            ),
-    ),
-  );
-}
+                  );
+                },
+              ),
+      ),
+    );
+  }
 
   String _formatDateTime(String dateTimeString) {
     DateTime dateTime = DateTime.parse(dateTimeString);
